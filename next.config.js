5bbec9d2d6e670f649750f47a4fd0a1372fcb700
NakaoYuya <ted0504@icloud.com>
2025-01-24 16:19:43 +0900

    mod

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.microcms-assets.io",
-//       },
-//     ],
-//   },
-//   /* config options here */
-//   reactStrictMode: false,
-// };
-
-// export default nextConfig;
-
-module.exports = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -32,5 +13,8 @@ module.exports = {
       },
     ],
   },
+  /* config options here */
   reactStrictMode: false,
 };
+
+export default nextConfig;
