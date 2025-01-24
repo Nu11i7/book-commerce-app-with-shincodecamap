2d15294976a26fbcd009c680d55f0d724fa8133a
NakaoYuya <ted0504@icloud.com>
2025-01-24 14:15:12 +0900

    mod

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient;
+let prisma: PrismaClient;
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient | undefined;
