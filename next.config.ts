a91dab7d95770e22840e9c4c7077d8accdea1c51
NakaoYuya <ted0504@icloud.com>
2025-01-24 13:27:55 +0900

    done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
   /* config options here */
+  reactStrictMode: false,
 };
 
 export default nextConfig;
